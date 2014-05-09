8900b7eac57ba78bd427e85ec6dbdd4d86eb54f5
TheBlueDog <thebluedog@gmail.com>
2014-05-09 14:52:08 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RubyLocoJobBoard::Application.config.secret_key_base = 'd04998cab76aad98d5d1cfd9e722184f125538617cbadda54fcec60fbbdbf5b20c4bcdc5758dede5b9fb6338f84362e4d3038cad842a0ad9e7ce6756cfc0b2f9'
